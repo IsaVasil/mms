bb87d6e08503ce8c69949bf9768141cc29e9bcff
IsaVasil <105173734+IsaVasil@users.noreply.github.com>
2022-07-17 22:18:21 +0300

    Add files via upload

---
+++ Day03zad1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+int a;
+int b;
+int c;
+int max;
+scanf("%o",&a);
+printf("%d",a);
+//scanf("%d",&b);
+//printf("%d",b);
+//scanf("%d",&c);
+//printf("%d",c);
+
+
+    return 0;
+}
